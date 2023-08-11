877bc2d890b5db0d3b8397298bd0a91072a5cdae
yoon <cbj0010@naver.com>
2023-08-12 02:43:14 +0900

    [chore/problem3] 함수분리
    나머지 값이 3,6,9에 해당할때랑 몫이 3,6,9에 해당하는 경우에 따른 함수 분리

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -3,23 +3,36 @@ package onboarding
 fun solution3(number: Int): Int {
     return checkGame(number)
 }
-
 fun checkGame(inputNum: Int): Int {
     var i = 1
     var count = 0
-    while (i < inputNum+1) {
+    while (i < inputNum + 1) {
         var j = i
         do {
             var divideTen = j % 10
-            if (divideTen.equals(3) || divideTen.equals(6) || divideTen.equals(9)) {
-                count++
+            when (checkRemainderNumber(divideTen)) {
+                1 -> count++
             }
-            if ((i/10).equals(3) || (i/10).equals(6) || (i/10).equals(9)){count++}
+            when (checkQuotientNumber(i)) {
+                1 -> count++
+            }
             j /= 10
         } while (j / 10 !== 0)
         i++
     }
     return count
+}
+
+fun checkRemainderNumber(remainder: Int): Int {
+    if (remainder.equals(3) || remainder.equals(6) || remainder.equals(9)) {
+        return 1
+    } else return 0
+}
+
+fun checkQuotientNumber(quotient: Int): Int {
+    if ((quotient / 10).equals(3) || (quotient / 10).equals(6) || (quotient / 10).equals(9)) {
+        return 1
+    } else return 0
 }
 /*
 입력받은 값 까지 하나씩 숫자를 올리며 각 자라의 값에 3,6,9가 있나 확인한다.
