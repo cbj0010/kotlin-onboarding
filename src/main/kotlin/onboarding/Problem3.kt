a916fb06d8b02ccbf306b1e8bf6e68a2594f62b2
yoon <cbj0010@naver.com>
2023-08-12 02:35:04 +0900

    [feat/problem3]

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,27 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    return checkGame(number)
 }
+
+fun checkGame(inputNum: Int): Int {
+    var i = 1
+    var count = 0
+    while (i < inputNum+1) {
+        var j = i
+        do {
+            var divideTen = j % 10
+            if (divideTen.equals(3) || divideTen.equals(6) || divideTen.equals(9)) {
+                count++
+            }
+            if ((i/10).equals(3) || (i/10).equals(6) || (i/10).equals(9)){count++}
+            j /= 10
+        } while (j / 10 !== 0)
+        i++
+    }
+    return count
+}
+/*
+입력받은 값 까지 하나씩 숫자를 올리며 각 자라의 값에 3,6,9가 있나 확인한다.
+같은 값이 있을 경우 숫자를 올린다
+ */
