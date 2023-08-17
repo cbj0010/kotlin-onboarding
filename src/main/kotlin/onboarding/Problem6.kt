f5c8af4cf420d1018a9edd8775b25f3ea9c4d18f
yoon <cbj0010@naver.com>
2023-08-17 20:39:20 +0900

    [feat/problem6]

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,35 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+//    val splitNameList = mutableMapOf<String, MutableSet<String>>()
+  return  splitName(forms)
 }
+
+private fun splitName(forms: List<List<String>>): List<String> {
+    val overLapName = mutableSetOf<String>()
+    for (index in forms.indices) {
+        val nickName = forms[index][1]
+        println("nickName $nickName")
+        for (i in 0..nickName.length - 2) {
+            val spliteName = nickName.substring(i, i + 2)
+            println("spliteName $spliteName")
+            for (j in 0 ..forms.size-1) {
+                if (nickName != forms[j][1] && forms[j][1].contains(spliteName)) {
+                    println("contains ${forms[j][1]}")
+                    overLapName.add(forms[j][0])
+                }
+            }
+            println()
+        }
+    }
+    return overLapName.sorted()
+}
+
+
+/*
+예외
+입력된 이메일의 길이는 11자 이상 20자 미만이다.
+신청할 수 있는 이메일은 eamil.com만 가능
+작업
+두자리씩 끊어서 이메일과 함께 저장
+ */
