0bc5fb136b47b7bbc9a0ff46ce10d6a623fd7157
yoon <cbj0010@naver.com>
2023-08-17 17:29:19 +0900

    [feat/problem4]
    청개구리 문제

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,42 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    return oppositionLetter(word)
 }
+
+fun changLetter(word: Char): Char {
+    if (word.isLowerCase()) {
+        return changeLowerCase(word)
+    } else if(word.isUpperCase()) {
+        return changeUpperCase(word)
+    }else{
+        return word
+    }
+}
+
+fun changeUpperCase(word: Char): Char {
+    return (155 - word.code).toChar()
+}
+
+fun changeLowerCase(word: Char): Char {
+    return (219 - word.code).toChar()
+}
+
+fun oppositionLetter(word: String): String {
+    var index = 0
+    var resultWord = ""
+    while (index in word.indices) {
+        resultWord += changLetter(word[index])
+        index++
+    }
+    return resultWord
+}
+
+/*
+입력한 값이 소문자인지 대문자인지 판단
+값의 반대를 return
+ */
+/*
+소문자 a = 97, z=122 25 12.5  109(M)이 중간
+A= 65 Z= 90 25차이 77(m)
+ */
